ef89f15adda3960fc8c1c08273dc30d9a9b08226
Krutarth Chovatiya <52130096+KrutarthChovatiya@users.noreply.github.com>
2025-09-25 10:48:01 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//   i18n: {
+//     locales: ["en"],
+//     defaultLocale: "en",
+//   },
+//   target: "serverless",
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback.fs = false;
+//     }
+//     return config;
+//   },
+// };
+
+const isProd = process.env.NODE_ENV === 'production';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    assetPrefix: isProd ? '/nextly/' : '',
+    basePath: isProd ? '/nextly' : '',
+};
+
+module.exports = nextConfig;

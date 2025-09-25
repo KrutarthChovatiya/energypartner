8b13cd350dee22e0aebd40be90bd9d59f7d49001
Krutarth Chovatiya <krutarthchovatiya4@gmail.com>
2025-09-25 12:59:57 +0530

    Fix asset paths and update basePath for GitHub Pages

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,14 @@ export default function Home() {
           name="description"
           content="Nextly is a free landing page template built with next.js & Tailwind CSS"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href={
+            process.env.NODE_ENV === "production"
+              ? "/energypartner/favicon.ico"
+              : "/favicon.ico"
+          }
+        />
       </Head>
 
       <Navbar />

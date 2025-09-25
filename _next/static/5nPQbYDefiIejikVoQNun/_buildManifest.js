bbfb9a4beafbdbcc5aeb233c32a45b564c7b82a5
Krutarth Chovatiya <krutarthchovatiya4@gmail.com>
2025-09-25 16:50:50 +0530

    Updates

--- _next/static/5nPQbYDefiIejikVoQNun/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/165-b9067608e1ea5c9ef349.js","static/chunks/pages/index-a04996d18001810393c4.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
